4595c846dd97e5128c483e5f92a2ce0cb12af41b
Melike Yurtoglu <aysemelikeyurtoglu@gmail.com>
2013-12-17 20:19:55 +0200

    Sayisal loto ilk dosya eklendi

---
+++ sayisalloto.py
@@ -0,0 +1,13 @@
+import random
+kolonlar = []
+for i in range(0,8):
+	loto = []
+	for a in range(0,6):
+		b =random.randrange(1,50)
+		loto.append(b)
+		if loto.count(b) == 0:
+		   loto.append(b)
+		   a = a-1
+		if kolonlar.count(loto) == 0:
+		   kolonlar.append(loto)
+print kolonlar
